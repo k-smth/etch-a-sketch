237d4bad87dce59fde66e9cbd4ad1d99be4e9d05
snackpacks <trvsh99@gmail.com>
2022-10-28 16:14:54 -0500

    added func that turns indiv square black when clicked

--- js.js
+++ js.js
@@ -1,13 +1,21 @@
 const container = document.querySelector('.container')
 
 function createDivs(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for(let i = 0; i < (row * cols); i++) {
+    // container.style.setProperty('--grid-rows', rows);
+    // container.style.setProperty('--grid-cols', cols);
+    for(let i = 0; i < (rows * cols); i++) {
         let div = document.createElement('div');
         div.classList.add('square');
         container.appendChild(div);
     }
 }
 
-createDivs(rows, cols);
+createDivs(16, 16);
+
+const squares = document.querySelectorAll('.square')
+
+squares.forEach(function(square) {
+    square.addEventListener('click', function changeColor(e) {
+        square.style.backgroundColor = 'black';
+    })
+});
